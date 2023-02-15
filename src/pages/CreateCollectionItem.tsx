022d856887b1fa5ad3d489773431a71264b32cb4
Dzmitry Dauhaliavets <dolgolevets21@gmail.com>
2023-02-15 18:56:58 +0300

    refactor: create item form

--- src/pages/CreateCollectionItem.tsx
+++ src/pages/CreateCollectionItem.tsx
@@ -2,17 +2,20 @@ import React from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import { useAppDispatch, useAppSelector } from '../store'
 import { useAdditionalFields } from '../hooks/useAdditionalFileds'
-import { Box, Button, TextField } from '@mui/material'
+import { Box, Button, TextField, Typography } from '@mui/material'
 import Stack from '@mui/material/Stack'
 import { ElementSwitcher } from '../components/shared/elementSwitcher/ElementSwitcher'
 import { createItem } from '../store/slices/itemsSlice/itemsSlice'
 import { PrevPageButton } from '../components/shared/buttons/PrevPageButton'
+import { Controller, SubmitHandler, useForm } from 'react-hook-form'
+
+type MainFields = {
+  title: string
+  tags: string
+}
 
 export const CreateCollectionItem = () => {
-  const [title, setTitle] = React.useState<string>('')
-  const [tags, setTags] = React.useState<string[]>([])
   const { collectionId } = useParams()
-
   const navigate = useNavigate()
   const dispatch = useAppDispatch()
 
@@ -23,61 +26,98 @@ export const CreateCollectionItem = () => {
   const { additionalFields, handleChangeAdditionalField } = useAdditionalFields(extraFields || [])
   const { user } = useAppSelector((state) => state.auth)
 
-  const onSubmit = () => {
-    if (title.length && tags.length && additionalFields?.every((field) => field.value !== '')) {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm({
+    defaultValues: {
+      title: '',
+      tags: '',
+    },
+  })
+
+  const onSubmit: SubmitHandler<MainFields> = (mainFields) => {
+    if (collectionId && user) {
       const newBody = {
-        collectionId: collectionId as string,
-        title,
-        tags,
+        collectionId,
+        title: mainFields.title,
+        tags: mainFields.tags.split(' '),
         extraFields: additionalFields,
       }
 
-      dispatch(createItem({ token: user?.token as string, body: newBody }))
+      dispatch(createItem({ token: user.token, body: newBody }))
       navigate(`/collections/${collectionId}`)
-    } else {
-      console.log('Sorry, your fields not filled')
     }
   }
 
   return (
-    <Stack spacing={3}>
+    <Stack spacing={2} sx={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
       <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
         <PrevPageButton />
-        <div>Create Collection Item</div>
       </Box>
-      <TextField
-        id='title-input'
-        label='Title'
-        variant='outlined'
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-        required
-      />
-      <TextField
-        id='tags-input'
-        label='Tags'
-        variant='outlined'
-        value={tags}
-        onChange={(e) => setTags([e.target.value])}
-        required
-      />
-
-      {additionalFields.map((field, i) => (
-        <Box
-          key={i}
-          py={1}
-          sx={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'space-between',
+      <Typography variant='h5' component='h5' align='center'>
+        Create Item
+      </Typography>
+      <Stack component={'form'} onSubmit={handleSubmit(onSubmit)} spacing={1}>
+        <Controller
+          name='title'
+          control={control}
+          rules={{
+            required: true,
           }}
+          render={({ field: { ref, ...field } }) => (
+            <TextField
+              {...field}
+              inputRef={ref}
+              id='title'
+              error={!!errors.title}
+              label='Title'
+              variant='outlined'
+              autoComplete='off'
+            />
+          )}
+        />
+        <Controller
+          name='tags'
+          control={control}
+          rules={{
+            required: true,
+          }}
+          render={({ field: { ref, ...field } }) => (
+            <TextField
+              {...field}
+              inputRef={ref}
+              id='tags'
+              error={!!errors.tags}
+              label='Tags'
+              variant='outlined'
+              autoComplete='off'
+            />
+          )}
+        />
+        {additionalFields.map((field, i) => (
+          <Box
+            key={i}
+            py={1}
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+            }}
+          >
+            <ElementSwitcher field={field} handleChange={handleChangeAdditionalField} />
+          </Box>
+        ))}
+        <Button
+          variant='contained'
+          type='submit'
+          disabled={!isValid}
+          sx={{ alignSelf: 'flex-end' }}
         >
-          <ElementSwitcher field={field} handleChange={handleChangeAdditionalField} />
-        </Box>
-      ))}
-      <Button variant='contained' onClick={onSubmit}>
-        Create
-      </Button>
+          Create
+        </Button>
+      </Stack>
     </Stack>
   )
 }
